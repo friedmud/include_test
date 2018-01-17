0279c2aad63d80a4f03a7e36da87792bc29a720d
Derek Gaston <derek.gaston@inl.gov>
2018-01-16 21:36:59 -0700

    Almost working - but too many files for linking

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 include_dirs := $(shell find projects/*/include -type d)
-CPPFLAGS := $(foreach i, $(include_dirs), -I$(i)) -std=c++14
+include_flags := $(foreach i, $(include_dirs), -I$(i))
+
+CPPFLAGS := -std=c++14 $(include_flags)
 
 srcfiles  := $(shell find projects -name "*.C")
 
 objects	:= $(patsubst %.C, %.o, $(srcfiles))
 
 main: main.C $(objects)
+
+clean:
+	$(foreach i, $(objects), $(shell rm -f $(i)))
+	rm -f main
--- create_dirs.py
+++ create_dirs.py
@@ -1,13 +1,18 @@
 import os
 from string import Template
+from random import randint
+
+num_apps = 10
+num_systems = 30
+num_files = 20
+num_includes = 5
 
 top_dir = 'projects'
 
-main_dirs = ['moose','misc','phase_field','chemical_reactions','fluid_properties','module_loader','porous_flow','solid_mechanics','water_steam_eos','combined','heat_conduction','rdg','stochastic_tools','xfem','contact','level_set','navier_stokes','richards','tensor_mechanics']
+app_dirs = ['app' + str(i) for i in xrange(num_apps)]
 
-sub_dirs = ['actions','dampers','geomsearch','materials','parser','samplers','utils','auxkernels','dgkernels','ics','mesh','partitioner','splits','vectorpostprocessors','base','dirackernels','indicators','meshmodifiers','postprocessors','timeintegrators','bcs','distributions','interfacekernels','multiapps','preconditioners','timesteppers','constraints','executioners','kernels','nodalkernels','predictors','transfers','controls','functions','markers','outputs','restart','userobject']
+system_dirs = ['system' + str(i) for i in xrange(num_systems)]
 
-num_files = 20
 
 header_template = """#ifndef ${classname}_h
 #define ${classname}_h
@@ -22,17 +27,20 @@ public:
 """
 
 source_template = """#include"$headerfile"
-namespace
+
+$includes
+
+void ${classname}_func()
 {
   $classname stuff;
   stuff.dumb = 4;
 }
 """
 
-for mdir in main_dirs:
-    for sdir in sub_dirs:
-        include_path = os.path.join(top_dir, mdir, 'include', sdir)
-        source_path = os.path.join(top_dir, mdir, 'src', sdir)
+for adir in app_dirs:
+    for sdir in system_dirs:
+        include_path = os.path.join(top_dir, adir, 'include', sdir)
+        source_path = os.path.join(top_dir, adir, 'src', sdir)
 
         try:
             os.makedirs(include_path)
@@ -40,14 +48,19 @@ for mdir in main_dirs:
         except:
             pass
 
-        for i in xrange(num_files):
-            classname = sdir + str(i)
+        classnames = [adir + sdir + 'class' + str(i) for i in xrange(num_files)]
+
+        for classname in classnames:
             header_filename = classname + '.h'
             source_filename = classname + '.C'
 
+            random_includes = ['#include"' + 'app' + str(randint(0, num_apps-1)) + 'system' + str(randint(0, num_systems-1)) + 'class' + str(randint(0, num_files-1)) + '.h"' for i in xrange(num_includes)]
+
+            includes = '\n'.join(random_includes)
+
             header_content = Template(header_template).substitute(classname=classname)
 
-            source_content = Template(source_template).substitute(headerfile=header_filename, classname=classname);
+            source_content = Template(source_template).substitute(headerfile=header_filename, classname=classname, includes=includes);
 
             header_file = open(os.path.join(include_path, header_filename), 'w')
             header_file.write(header_content)
